87330802938cfd03659cdc277d53f6093ba7f836
Allan <allan.kimurgor@student.moringaschool.com>
2022-09-08 16:15:54 +0300

    The function convert passes the user inputs to API and conversions will happen using API and fetches and displays result

--- script.js
+++ script.js
@@ -32,12 +32,12 @@ btn.addEventListener("click", () => {
 
 function convert(currency1, currency2, value){
     const host = "api.frankfurter.app";
-//     fetch(
-//         `https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`
-//     )
-//     .then((val) => val.json())
-//     .then((val) =>{
-//         console.log(Object.values(val.rates)[0]);
-//         result.value = Object.values(val.rates)[0];
-//     })
+    fetch(
+        `https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`
+    )
+    .then((val) => val.json())
+    .then((val) =>{
+        console.log(Object.values(val.rates)[0]);
+        result.value = Object.values(val.rates)[0];
+    })
 }
